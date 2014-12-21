e5459b7fa53deaefb4ed2068716c51803cbf3c17
Cristian Romanescu <cristian@romanescu.ro>
2014-12-21 18:25:44 +0200

    Removed double archiving for usr directory

--- etc/backup.d/60-system.sh
+++ etc/backup.d/60-system.sh
@@ -29,11 +29,6 @@ if [ $? -ne 0 ]; then
 	error "system: Cannot backup /usr!"
 fi
 
-tar cvfz "$backup/usr.tar.gz" /usr > /dev/null 2>&1
-if [ $? -ne 0 ]; then
-	error "system: Cannot backup /usr!"
-fi
-
 # /var/www - backup separately
 # /var/log - backup separately
 tar cvfz "$backup/var.tar.gz" /var --exclude=/var/lib/mysql --exclude=/var/tmp --exclude=/var/lock --exclude=/var/cache --exclude=/var/www --exclude=/var/log > /dev/null 2>&1
